08263cb6114bc10fdda8f03fdd78d22f62c32ac2
Matheus Ferreira <matheus.ferreira@bandtec.com.br>
2025-04-21 20:07:46 -0300

    feat: add shopping cart without SOLID

--- app_carrinho_compras/index.php
+++ app_carrinho_compras/index.php
@@ -4,4 +4,27 @@ require __DIR__ . "/vendor/autoload.php";
 
 use App\CarrinhoCompra;
 
-echo CarrinhoCompra::test();
+$carrinho1 = new CarrinhoCompra();
+print_r($carrinho1->getItems());
+echo 'Valor Total: ' . $carrinho1->getTotal();
+
+$carrinho1->addItem('Bicicleta', 750.10);
+$carrinho1->addItem('Geladeira', 1950.99);
+$carrinho1->addItem('Tapete', 350.00);
+
+echo "<br />";
+print_r($carrinho1->getItems());
+echo 'Valor Total recalculado: ' . $carrinho1->getTotal();
+
+echo "<br />";
+echo 'status: ' . $carrinho1->getStatus();
+
+echo "<br />";
+if ($carrinho1->confirmOrder()) {
+    echo 'Pedido realizado com sucesso!';
+} else {
+    echo 'Erro na confirmação do pedido.';
+}
+
+echo "<br />";
+echo 'status: ' . $carrinho1->getStatus();
--- app_carrinho_compras/src/CarrinhoCompra.php
+++ app_carrinho_compras/src/CarrinhoCompra.php
@@ -4,8 +4,59 @@ namespace App;
 
 class CarrinhoCompra
 {
-    public static function test()
+    // attributes
+    private $items;
+    private $status;
+    private $total;
+
+    // methods
+    public function __construct()
     {
-        return 'ok';
+        $this->items = [];
+        $this->status = 'open';
+        $this->total = 0;
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function addItem(string $item, float $value)
+    {
+        array_push($this->items, ["item" => $item, "value" => $value]);
+        $this->total += $value;
+        return true;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function confirmOrder()
+    {
+        if ($this->validateCart()) {
+            $this->status = 'closed';
+            $this->sendConfirmationEmail();
+
+            return true;
+        }
+        return false;
+    }
+
+    public function sendConfirmationEmail()
+    {
+        echo '<br />... envia e-mail de confirmação ... <br />';
+    }
+
+    public function validateCart()
+    {
+        return count($this->items) > 0;
     }
 }
